564d5210448ad8ebd0cde07655cac7fc68b03790
Developer SCS <99910650+DEV-SCS@users.noreply.github.com>
2023-02-24 15:26:20 +0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,16 @@
+ALTER Function [dbo].[Fn_aratarihler](@tarih1 date,@tarih2 date)
+Returns nvarchar(MAX)
+As
+Begin
+	DECLARE @tarihler nvarchar(MAX)= '';
+
+	SET @tarih1 = DATEADD(day,1,@tarih1);
+	WHILE @tarih1 < @tarih2 
+	BEGIN
+		SET @tarihler+=(case when @tarihler='' then '' else ',' end)+concat(year(@tarih1) ,'-' ,month(@tarih1),'-', day(@tarih1))
+		SET @tarih1 = DATEADD(day,1,@tarih1);
+	END;
+
+
+	return @tarihler
+End
